da784fc559fbec3bfddd9ba160e64dff1ffcee48
zzs <zzs@weyee.com>
2017-09-13 17:11:05 +0800

    add

--- index.php
+++ index.php
@@ -14,34 +14,8 @@
 <body>
 
 <div class="container" style="padding-top: 60px;">
-    <h1>Tonight	Schedule</h1>
-    <div>
-        <img src="aa.jpg"  style="width: 50%;height: 500px">
-    </div>
-    <div>
-        <div class="form-group">
-            <label for="exampleInputEmail1"><h3>Would you like to have dinner with me tonight?</h3></label>
-            <br>
-            <button class="btn btn-success" id="success">yes</button>
-            <br>
-            <br>
-            <button type="button" class="btn btn-danger"  data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Right?" id="lose">no</button>
-        </div>
-    </div>
 
 </div>
-<script>
-    $(function () {
-     $('#success').on('click',function () {
-         alert("good hoopier baby!!");
-     });
-      $('#lose').on('click',function () {
-          alert("eat what tonight!!");
-      });
-    });
-
-</script>
-
 </body>
 </html>
 
